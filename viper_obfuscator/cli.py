f3f964b010d36a8b51382ecb1ed1ef7736ffd9f6
Think0 <think0btw@gmail.com>
2026-01-23 22:38:43 +0000

    Implement clear, help, and manual path functions
    Added functions for clearing the screen, showing help, and manual file path entry.

--- viper_obfuscator/cli.py
+++ viper_obfuscator/cli.py
@@ -37,10 +37,15 @@ ascii_art = r"""
             ~ Nat11-n1                   ⡇⠀⠀⠳⡊⠐⢺⠄⣐⣋⣗⠉⠀⠀⠀⢀⣀⣀⡀⠀⡇⠀⠀⠱⡀
                                          ⠑⢄⠀⠀⠀⠀⠀⢣⠀⠈⢛⠣⢤⡖⠊⠉⠀⠀⣀⠠⠂⠀⠀⢰⠁
                                          ⠀⠀⠉⠒⠠⠤⠤⠖⠓⢄⡀⠁⠀⠀⠈⠉⠁⠀⠀⠀⠀⢀⡴⠃⠀
- use --help for help                     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠒⠒⠀⠀⠀⠒⠒⠒⠉⠁⠀⠀⠀  """
+ use help for help                       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠒⠒⠀⠀⠀⠒⠒⠒⠉⠁⠀⠀⠀  """
 
 print(color(ascii_art))
 
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    
 def drag_file():
     path = input("Glisse un fichier ici : ").strip().strip('"').strip("'")
 
@@ -50,15 +55,60 @@ def drag_file():
         print("Not a python file")
     return
 
+def mlanualpath():
+    path = input("Enter the file path : ")
+    if path.lower().endswith(".py") and os.path.isfile(path):
+        print("Path :", path)
+    else:
+        print("Not a python file")
+    return
+
+def help():
+    print(color("""
+    help : show this help
+    drag : drag and drop a python file
+    clear : clear the screen
+    path : show the current path
+    manualpath : manually enter a file path
+    exit : exit the program
+    """))
 
 
 while True:
     cmd =input(color("[>]  "))
     if cmd =='drag':
         drag_file()
+    if cmd =='help':
+        help()
+    if cmd =='clear':
+        clear()
+    if cmd =='path':
+        print(os.getcwd())
+    if cmd =='manualpath':
+        mlanualpath()
 
 
 
 
 
+
+
+
+
+def inutile ():
+    enter = input(purple("Enter the text to convert to base64: "))
+    encode =base64.b64encode(enter.encode("utf-8"))
+    print(purple(f"Encoded text: {encode.decode('utf-8')}"))
+    marshalled_code = marshal.dumps(encode)
+    with open("encoded_script.py", "w") as file:
+        file.write("import base64, marshal\n")
+        file.write("data = ")
+        file.write(repr(marshalled_code))
+        file.write("\n")
+        file.write("decoded_data = marshal.loads(data)\n")
+        file.write("original_text = base64.b64decode(decoded_data).decode('utf-8')\n")
+        file.write("print('Decoded text:', original_text)\n")
+    print(purple("Encoded script saved to 'encoded_script.py'"))
+    init(autoreset=True)
+    print(Style.RESET_ALL)
 
