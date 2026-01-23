989a26c198936385f880824f01abb69b493002c0
Nat11-n1 <nat11n1n1@gmail.com>
2026-01-23 23:12:31 +0100

    Update obfuscator.py

--- viper_obfuscator/obfuscator.py
+++ viper_obfuscator/obfuscator.py
@@ -5,9 +5,19 @@ path=""
 
 def GetPATH():
     global path
-    path = input("Enter a valid .py PATH :")
+    UseDropFile = ""
+    UseDropFile = input("Do you want to use drop file ? y for yes : ")
+    if (UseDropFile == "y") :
+        os.startfile(os.getcwd())
+        path = input("Glisse un fichier ici : ").strip('"')
+        print(path)
+    else :
+        path = input("Enter a valid .py PATH : ")
+
     if path.endswith('.py'):
         return path
     else :
         print("This PATH isn't valid")
         GetPATH()
+
+GetPATH()
