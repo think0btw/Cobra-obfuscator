b9f781e6c0400c69d299e10a3c8f0fad2537c75c
Nat11-n1 <nat11n1n1@gmail.com>
2026-01-25 16:15:03 +0100

    Update obfuscator.py

--- viper_obfuscator/obfuscator.py
+++ viper_obfuscator/obfuscator.py
@@ -1,11 +1,10 @@
 #obfuscator
-import marshal ,base64 ,os
+import base64 ,os ,xor
 
 def obfuscate(PATH):
     with open(PATH,"r",encoding="utf-8") as f:
         source = f.read()
         encode =base64.b64encode(f.encode("utf-8"))
-    marshalled_code = marshal.dumps(encode)
     
     
 def compilefile(PATH):
