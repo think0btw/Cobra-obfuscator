9ee7a1d4d568f71f11f7c2825c075982518f9bc3
Nat11-n1 <nat11n1n1@gmail.com>
2026-01-25 15:30:19 +0100

    Update cli.py

--- viper_obfuscator/cli.py
+++ viper_obfuscator/cli.py
@@ -1,4 +1,4 @@
-import marshal , os , base64
+import os
 
 os.system("")
 
@@ -88,29 +88,3 @@ while True:
         mlanualpath()
     if cmd =='exit':
         break
-
-
-
-
-
-
-
-
-
-def inutile ():
-    enter = input(purple("Enter the text to convert to base64: "))
-    encode =base64.b64encode(enter.encode("utf-8"))
-    print(purple(f"Encoded text: {encode.decode('utf-8')}"))
-    marshalled_code = marshal.dumps(encode)
-    with open("encoded_script.py", "w") as file:
-        file.write("import base64, marshal\n")
-        file.write("data = ")
-        file.write(repr(marshalled_code))
-        file.write("\n")
-        file.write("decoded_data = marshal.loads(data)\n")
-        file.write("original_text = base64.b64decode(decoded_data).decode('utf-8')\n")
-        file.write("print('Decoded text:', original_text)\n")
-    print(purple("Encoded script saved to 'encoded_script.py'"))
-    init(autoreset=True)
-    print(Style.RESET_ALL)
-
