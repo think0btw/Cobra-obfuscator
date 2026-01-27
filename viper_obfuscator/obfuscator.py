5d00a7c70745cfe892436846d5b20b361d461310
Nat11-n1 <nat11n1n1@gmail.com>
2026-01-27 18:16:02 +0100

    Update obfuscator.py

--- viper_obfuscator/obfuscator.py
+++ viper_obfuscator/obfuscator.py
@@ -4,8 +4,13 @@ import base64 ,os
 def obfuscate(PATH):
     with open(PATH,"r",encoding="utf-8") as f:
         source = f.read()
+        #base64
         encode =base64.b64encode(f.encode("utf-8"))
-    
+        #xor
+        key = os.urandom(16)
+        xor = bytearray()
+        for i in range(len(encode)):
+            xor.append(encode[i] ^ key[i % len(key)])
     
 def compilefile(PATH):
     with open(PATH,"r",encoding="utf-8") as f:
